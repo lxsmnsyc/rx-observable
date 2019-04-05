1b55f29ed9532563a5d55664b51b7fe59a0c9939
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-05 11:01:44 +0800

    Update operators.js

--- src/internal/operators.js
+++ src/internal/operators.js
@@ -7,6 +7,7 @@ export { default as delay } from './operator/delay';
 export { default as delaySubscription } from './operator/delaySubscription';
 export { default as error } from './operator/error';
 export { default as filter } from './operator/filter';
+export { default as just } from './operator/just';
 export { default as lift } from './operator/lift';
 export { default as map } from './operator/map';
 export { default as never } from './operator/never';
