5b0fe7e46932e601d49b0befc5a37dd29f415dbc
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-05 08:49:12 +0800

    Create create.js

---
+++ src/internal/operator/create.js
@@ -0,0 +1,44 @@
+import AbortController from 'abort-controller';
+import {
+  onErrorHandler, onNextHandler, cleanObserver, onCompleteHandler, isFunction,
+} from '../utils';
+import Observable from '../../observable';
+import error from './error';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onNext, onComplete, onError, onSubscribe,
+  } = cleanObserver(observer);
+
+  const emitter = new AbortController();
+  emitter.onComplete = onCompleteHandler.bind(this);
+  emitter.onNext = onNextHandler.bind(this);
+  emitter.onError = onErrorHandler.bind(this);
+
+  this.controller = emitter;
+  this.onComplete = onComplete;
+  this.onNext = onNext;
+  this.onError = onError;
+
+  onSubscribe(emitter);
+
+  try {
+    this.subscriber(emitter);
+  } catch (ex) {
+    emitter.onError(ex);
+  }
+}
+/**
+ * @ignore
+ */
+export default (subscriber) => {
+  if (!isFunction(subscriber)) {
+    return error(new Error('Observable.create: There are no subscribers.'));
+  }
+  const observable = new Observable(subscribeActual);
+  observable.subscriber = subscriber;
+  return observable;
+};
