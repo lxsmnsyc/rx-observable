26f82eaf59d4dd068df6e8da3c1363cc1d30ee20
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-05 08:49:18 +0800

    Create utils.js

---
+++ src/internal/utils.js
@@ -0,0 +1,133 @@
+import AbortController from 'abort-controller';
+/**
+ * @ignore
+ */
+// eslint-disable-next-line valid-typeof
+const isType = (x, y) => typeof x === y;
+/**
+ * @ignore
+ */
+export const isFunction = x => isType(x, 'function');
+/**
+ * @ignore
+ */
+export const isNumber = x => isType(x, 'number');
+/**
+ * @ignore
+ */
+export const isObject = x => isType(x, 'object');
+/**
+ * @ignore
+ */
+export const isIterable = obj => isObject(obj) && isFunction(obj[Symbol.iterator]);
+/**
+ * @ignore
+ */
+export const isObserver = obj => isObject(obj) && isFunction(obj.onSubscribe);
+/**
+ * @ignore
+ */
+export const toCallable = x => () => x;
+/**
+ * @ignore
+ */
+export const isPromise = (obj) => {
+  if (obj == null) return false;
+  if (obj instanceof Promise) return true;
+  return (isObject(obj) || isFunction(obj)) && isFunction(obj.then);
+};
+/**
+ * @ignore
+ */
+export function onNextHandler(value) {
+  const { onNext, onError, controller } = this;
+  if (controller.signal.aborted) {
+    return;
+  }
+  try {
+    if (value == null) {
+      onError('onNext called with null value.');
+    } else {
+      onNext(value);
+    }
+  } catch (e) {
+    onError(e);
+  }
+}
+/**
+ * @ignore
+ */
+export function onCompleteHandler() {
+  const { onComplete, controller } = this;
+  if (controller.signal.aborted) {
+    return;
+  }
+  try {
+    onComplete();
+  } finally {
+    controller.abort();
+  }
+}
+/**
+ * @ignore
+ */
+export function onErrorHandler(err) {
+  const { onError, controller } = this;
+  let report = err;
+  if (!(err instanceof Error)) {
+    report = new Error('onError called with a non-Error value.');
+  }
+  if (controller.signal.aborted) {
+    return;
+  }
+
+  try {
+    onError(report);
+  } finally {
+    controller.abort();
+  }
+}
+/**
+ * @ignore
+ */
+const identity = x => x;
+/**
+ * @ignore
+ */
+const throwError = (x) => { throw x; };
+/**
+ * @ignore
+ */
+export const cleanObserver = x => ({
+  onSubscribe: x.onSubscribe,
+  onNext: isFunction(x.onNext) ? x.onNext : identity,
+  onComplete: isFunction(x.onComplete) ? x.onComplete : identity,
+  onError: isFunction(x.onError) ? x.onError : throwError,
+});
+/**
+ * @ignore
+ */
+export const immediateComplete = (o) => {
+  // const disposable = new SimpleDisposable();
+  const { onSubscribe, onComplete } = cleanObserver(o);
+  const controller = new AbortController();
+  onSubscribe(controller);
+
+  if (!controller.signal.aborted) {
+    onComplete();
+    controller.abort();
+  }
+};
+/**
+ * @ignore
+ */
+export const immediateError = (o, x) => {
+  const { onSubscribe, onError } = cleanObserver(o);
+  const controller = new AbortController();
+  onSubscribe(controller);
+
+  if (!controller.signal.aborted) {
+    onError(x);
+    controller.abort();
+  }
+};
