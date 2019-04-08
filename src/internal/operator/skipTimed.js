43958c063bf61366b8ee06744c64868e00236748
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-08 14:23:31 +0800

    Create skipTimed.js

---
+++ src/internal/operator/skipTimed.js
@@ -0,0 +1,59 @@
+import AbortController from 'abort-controller';
+import Observable from '../../observable';
+import { isNumber, cleanObserver } from '../utils';
+
+
+function subscribeActual(observer) {
+  const {
+    onSubscribe, onNext, onError, onComplete,
+  } = cleanObserver(observer);
+
+  const controller = new AbortController();
+
+  onSubscribe(controller);
+
+  const { signal } = controller;
+
+  if (signal.aborted) {
+    return;
+  }
+
+  const { source, amount } = this;
+
+  let expired = false;
+
+  const timer = setTimeout(() => {
+    expired = true;
+  }, amount);
+
+  signal.addEventListener('abort', () => clearTimeout(timer));
+
+  source.subscribeWith({
+    onSubscribe(ac) {
+      signal.addEventListener('abort', () => ac.abort());
+    },
+    onComplete() {
+      onComplete();
+      controller.abort();
+    },
+    onError(x) {
+      onError(x);
+      controller.abort();
+    },
+    onNext(x) {
+      if (expired) {
+        onNext(x);
+      }
+    },
+  });
+}
+
+export default (source, amount) => {
+  if (!isNumber(amount)) {
+    return source;
+  }
+  const observable = new Observable(subscribeActual);
+  observable.amount = amount;
+  observable.source = source;
+  return observable;
+};
