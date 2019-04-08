ac79458e57866bf8ca0e606123f4b8f53e0a7f23
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-08 13:21:59 +0800

    Create takeTimed.js

---
+++ src/internal/operator/takeTimed.js
@@ -0,0 +1,53 @@
+import AbortController from 'abort-controller';
+import Observable from '../../observable';
+import { isNumber, cleanObserver } from '../utils';
+
+
+function subscribeActual(observer) {
+  const {
+    onSubscribe, onNext, onError, onComplete,
+  } = cleanObserver(observer);
+
+  const controller = new AbortController();
+
+  onSubscribe(controller);
+
+  const { signal } = controller;
+
+  if (signal.aborted) {
+    return;
+  }
+
+  const { source, amount } = this;
+
+  const timer = setTimeout(() => {
+    onComplete();
+    controller.abort();
+  }, amount);
+
+  signal.addEventListener('abort', () => clearTimeout(timer));
+  source.subscribeWith({
+    onSubscribe(ac) {
+      signal.addEventListener('abort', () => ac.abort());
+    },
+    onComplete() {
+      onComplete();
+      controller.abort();
+    },
+    onError(x) {
+      onError(x);
+      controller.abort();
+    },
+    onNext,
+  });
+}
+
+export default (source, amount) => {
+  if (!isNumber(amount)) {
+    return source;
+  }
+  const observable = new Observable(subscribeActual);
+  observable.amount = amount;
+  observable.source = source;
+  return observable;
+};
