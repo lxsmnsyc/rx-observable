0100bd7d7dc995ea6ff569f680b0df583c8cd604
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-08 13:51:18 +0800

    Update operators.js

--- src/internal/operators.js
+++ src/internal/operators.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-cycle */
 export { default as amb } from './operator/amb';
 export { default as ambWith } from './operator/ambWith';
 export { default as compose } from './operator/compose';
@@ -10,4 +11,10 @@ export { default as filter } from './operator/filter';
 export { default as just } from './operator/just';
 export { default as lift } from './operator/lift';
 export { default as map } from './operator/map';
+export { default as merge } from './operator/merge';
+export { default as mergeDelayError } from './operator/mergeDelayError';
+export { default as mergeWith } from './operator/mergeWith';
 export { default as never } from './operator/never';
+export { default as take } from './operator/take';
+export { default as takeLast } from './operator/takeLast';
+export { default as takeTimed } from './operator/takeTimed';
