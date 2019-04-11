c09574ad13bb28a6e10f9c8df8c8f2e027a7111c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-11 10:07:15 +0800

    Update concatDelayError.js

--- src/internal/operator/concatDelayError.js
+++ src/internal/operator/concatDelayError.js
@@ -31,6 +31,10 @@ function subscribeActual(observer) {
     if (!(source instanceof Observable)) {
       onError(new Error('Observable.concatDelayError: one of the sources is a non-Observable.'));
       controller.abort();
+      return;
+    }
+    if (signal.aborted) {
+      return;
     }
     counter += 1;
     source.subscribeWith({
