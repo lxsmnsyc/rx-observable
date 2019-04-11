6627c9f9df1d1575dc2989300684972c8efe4109
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-11 08:51:26 +0800

    Create concatWith.js

---
+++ src/internal/operator/concatWith.js
@@ -0,0 +1,11 @@
+import Observable from '../../observable';
+import concat from './concat';
+/**
+ * @ignore
+ */
+export default (source, other) => {
+  if (!(other instanceof Observable)) {
+    return source;
+  }
+  return concat([source, other]);
+};
