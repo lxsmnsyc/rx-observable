1d69c34f43bd3fa1b3babf0f161baf5d9a36147b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-11 10:06:59 +0800

    Update delaySubscription.js

--- src/internal/operator/delaySubscription.js
+++ src/internal/operator/delaySubscription.js
@@ -1,4 +1,5 @@
 import AbortController from 'abort-controller';
+import Scheduler from 'rx-scheduler';
 import Observable from '../../observable';
 import { cleanObserver, isNumber } from '../utils';
 
@@ -10,8 +11,7 @@ function subscribeActual(observer) {
     onNext, onComplete, onError, onSubscribe,
   } = cleanObserver(observer);
 
-  const { amount } = this;
-
+  const { amount, scheduler } = this;
   const controller = new AbortController();
 
   const { signal } = controller;
@@ -22,7 +22,7 @@ function subscribeActual(observer) {
     return;
   }
 
-  const timeout = setTimeout(() => {
+  const abortable = scheduler.delay(() => {
     this.source.subscribeWith({
       onSubscribe(ac) {
         signal.addEventListener('abort', () => ac.abort());
@@ -39,19 +39,22 @@ function subscribeActual(observer) {
     });
   }, amount);
 
-  signal.addEventListener('abort', () => {
-    clearTimeout(timeout);
-  });
+  signal.addEventListener('abort', () => abortable.abort());
 }
 /**
  * @ignore
  */
-export default (source, amount) => {
+export default (source, amount, scheduler) => {
   if (!isNumber(amount)) {
     return source;
+  }
+  let sched = scheduler;
+  if (!(sched instanceof Scheduler.interface)) {
+    sched = Scheduler.current;
   }
   const observable = new Observable(subscribeActual);
   observable.source = source;
   observable.amount = amount;
+  observable.scheduler = sched;
   return observable;
 };
