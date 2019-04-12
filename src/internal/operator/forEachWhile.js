b70cbe41deab9d003ce05feaba998c66e0b49481
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 17:35:46 +0800

    Create forEachWhile.js

---
+++ src/internal/operator/forEachWhile.js
@@ -0,0 +1,31 @@
+import { LinkedCancellable } from 'rx-cancellable';
+import { isFunction } from '../utils';
+/**
+ * @ignore
+ */
+export default (source, predicate) => {
+  const controller = new LinkedCancellable();
+
+  if (!isFunction(predicate)) {
+    controller.cancel();
+  } else {
+    source.subscribeWith({
+      onSubcribe(ac) {
+        controller.link(ac);
+      },
+      onComplete() {
+        controller.cancel();
+      },
+      onError(x) {
+        controller.cancel();
+        throw x;
+      },
+      onNext(x) {
+        if (!predicate(x)) {
+          controller.cancel();
+        }
+      },
+    });
+  }
+  return controller;
+};
