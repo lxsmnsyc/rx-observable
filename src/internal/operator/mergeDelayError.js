d811ece14976d9bf4eda18c0ace78392a8650fa2
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 17:35:52 +0800

    Update mergeDelayError.js

--- src/internal/operator/mergeDelayError.js
+++ src/internal/operator/mergeDelayError.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-loop-func */
 /* eslint-disable no-restricted-syntax */
-import AbortController from 'abort-controller';
+import { CompositeCancellable } from 'rx-cancellable';
 import Observable from '../../observable';
 import { isIterable, cleanObserver } from '../utils';
 import error from './error';
@@ -13,34 +13,20 @@ function subscribeActual(observer) {
     onNext, onComplete, onError, onSubscribe,
   } = cleanObserver(observer);
 
-  const controller = new AbortController();
-
-  const { signal } = controller;
+  const controller = new CompositeCancellable();
 
   onSubscribe(controller);
 
-  if (signal.aborted) {
-    return;
-  }
-
   const { sources } = this;
-
-  const controllers = [];
-
-  signal.addEventListener('abort', () => controllers.forEach(x => x.abort()));
-
   let completed = sources.length;
 
   const errors = [];
 
   for (const observable of sources) {
-    if (signal.aborted) {
-      return;
-    }
     if (observable instanceof Observable) {
       observable.subscribeWith({
         onSubscribe(ac) {
-          controllers.push(ac);
+          controller.add(ac);
         },
         onComplete() {
           completed -= 1;
@@ -50,7 +36,7 @@ function subscribeActual(observer) {
             } else {
               onError(errors);
             }
-            controller.abort();
+            controller.cancel();
           }
         },
         onError(x) {
@@ -58,7 +44,7 @@ function subscribeActual(observer) {
           completed -= 1;
           if (completed === 0) {
             onError(errors);
-            controller.abort();
+            controller.cancel();
           }
         },
         onNext,
