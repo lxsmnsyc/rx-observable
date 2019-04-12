535ca71199f92123d341c87f2fc356df135ec492
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 17:35:34 +0800

    Update concatDelayError.js

--- src/internal/operator/concatDelayError.js
+++ src/internal/operator/concatDelayError.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-syntax */
-import AbortController from 'abort-controller';
+import { LinkedCancellable } from 'rx-cancellable';
 import Observable from '../../observable';
 import { cleanObserver, isIterable } from '../utils';
 import error from './error';
@@ -9,15 +9,9 @@ function subscribeActual(observer) {
     onSubscribe, onNext, onError, onComplete,
   } = cleanObserver(observer);
 
-  const controller = new AbortController();
+  const controller = new LinkedCancellable();
 
   onSubscribe(controller);
-
-  const { signal } = controller;
-
-  if (signal.aborted) {
-    return;
-  }
 
   const { sources } = this;
   const { length } = sources;
@@ -30,16 +24,14 @@ function subscribeActual(observer) {
     const source = sources[counter];
     if (!(source instanceof Observable)) {
       onError(new Error('Observable.concatDelayError: one of the sources is a non-Observable.'));
-      controller.abort();
+      controller.cancel();
       return;
     }
-    if (signal.aborted) {
-      return;
-    }
+    controller.unlink();
     counter += 1;
     source.subscribeWith({
       onSubscribe(ac) {
-        signal.addEventListener('abort', () => ac.abort());
+        controller.link(ac);
       },
       onComplete() {
         if (counter === length) {
@@ -48,7 +40,6 @@ function subscribeActual(observer) {
           } else {
             onError(errors);
           }
-          controller.abort();
         } else {
           sub();
         }
