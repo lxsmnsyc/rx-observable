9da5710b8c1c8a8a8e4db87e7d67130ed2d8dcac
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 17:35:50 +0800

    Update merge.js

--- src/internal/operator/merge.js
+++ src/internal/operator/merge.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-loop-func */
 /* eslint-disable no-restricted-syntax */
-import AbortController from 'abort-controller';
+import { CompositeCancellable } from 'rx-cancellable';
 import Observable from '../../observable';
 import { isIterable, cleanObserver } from '../utils';
 import error from './error';
@@ -13,49 +13,35 @@ function subscribeActual(observer) {
     onNext, onComplete, onError, onSubscribe,
   } = cleanObserver(observer);
 
-  const controller = new AbortController();
-
-  const { signal } = controller;
+  const controller = new CompositeCancellable();
 
   onSubscribe(controller);
 
-  if (signal.aborted) {
-    return;
-  }
-
   const { sources } = this;
-
-  const controllers = [];
-
-  signal.addEventListener('abort', () => controllers.forEach(x => x.abort()));
-
   let completed = sources.length;
 
   for (const observable of sources) {
-    if (signal.aborted) {
-      return;
-    }
     if (observable instanceof Observable) {
       observable.subscribeWith({
         onSubscribe(ac) {
-          controllers.push(ac);
+          controller.add(ac);
         },
         onComplete() {
           completed -= 1;
           if (completed === 0) {
             onComplete();
-            controller.abort();
+            controller.cancel();
           }
         },
         onError(x) {
           onError(x);
-          controller.abort();
+          controller.cancel();
         },
         onNext,
       });
     } else {
       onError(new Error('Observable.merge: One of the sources is a non-Observable.'));
-      controller.abort();
+      controller.cancel();
       break;
     }
   }
