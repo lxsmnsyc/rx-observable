bf33cc71ee76a8cfc77c88bcd14a1136b54de3db
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 18:02:18 +0800

    Create repeat.js

---
+++ src/internal/operator/repeat.js
@@ -0,0 +1,65 @@
+import { LinkedCancellable } from 'rx-cancellable';
+import Observable from '../../observable';
+import { cleanObserver, isNumber } from '../utils';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onSubscribe, onNext, onComplete, onError,
+  } = cleanObserver(observer);
+
+  const controller = new LinkedCancellable();
+
+  onSubscribe(controller);
+
+  const { source, times } = this;
+
+  let retries = -1;
+
+  const sub = () => {
+    controller.unlink();
+    retries += 1;
+
+    source.subscribeWith({
+      onSubscribe(ac) {
+        controller.link(ac);
+      },
+      onComplete() {
+        if (isNumber(times)) {
+          if (retries <= times) {
+            sub();
+          } else {
+            onComplete();
+            controller.cancel();
+          }
+        } else {
+          sub();
+        }
+      },
+      onError,
+      onNext,
+    });
+  };
+
+  sub();
+}
+
+/**
+ * @ignore
+ */
+export default (source, times) => {
+  if (times != null) {
+    if (!isNumber(times)) {
+      return source;
+    }
+    if (times <= 0) {
+      return source;
+    }
+  }
+  const observable = new Observable(subscribeActual);
+  observable.source = source;
+  observable.times = times;
+  return observable;
+};
