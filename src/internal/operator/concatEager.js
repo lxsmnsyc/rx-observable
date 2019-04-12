ede046f69fcf81a79dc25e525c113ef5282d714c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 17:35:36 +0800

    Create concatEager.js

---
+++ src/internal/operator/concatEager.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-loop-func */
+/* eslint-disable no-restricted-syntax */
+import { CompositeCancellable } from 'rx-cancellable';
+import Observable from '../../observable';
+import { cleanObserver, isIterable } from '../utils';
+import error from './error';
+
+function subscribeActual(observer) {
+  const {
+    onSubscribe, onNext, onError, onComplete,
+  } = cleanObserver(observer);
+
+  const controller = new CompositeCancellable();
+
+  onSubscribe(controller);
+
+  const { signal } = controller;
+
+  if (signal.aborted) {
+    return;
+  }
+
+  const { sources } = this;
+  const { length } = sources;
+
+  let counter = 0;
+
+  const parentBuffer = [];
+
+
+  for (const source of sources) {
+    if (!(source instanceof Observable)) {
+      onError(new Error('Observable.concatEager: one of the sources is a non-Observable.'));
+      controller.cancel();
+      return;
+    }
+    const buffer = [];
+
+    parentBuffer.push(buffer);
+
+    source.subscribeWith({
+      onSubscribe(ac) {
+        controller.link(ac);
+      },
+      onComplete() {
+        counter += 1;
+
+        if (counter === length) {
+          for (const b of parentBuffer) {
+            for (const i of b) {
+              onNext(i);
+            }
+          }
+          onComplete();
+        }
+        controller.cancel();
+      },
+      onError(x) {
+        onError(x);
+        controller.cancel();
+      },
+      onNext(x) {
+        buffer.push(x);
+      },
+    });
+  }
+}
+/**
+ * @ignore
+ */
+export default (sources) => {
+  if (!isIterable(sources)) {
+    return error(new Error('Observable.concatEager: sources is not Iterable.'));
+  }
+  const observable = new Observable(subscribeActual);
+  observable.sources = sources;
+  return observable;
+};
