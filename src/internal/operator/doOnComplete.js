e8f05c401229b7595b2bb2c429745aea7488feb6
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-15 10:39:56 +0800

    Create doOnComplete.js

---
+++ src/internal/operator/doOnComplete.js
@@ -0,0 +1,36 @@
+import Observable from '../../observable';
+import { cleanObserver, isFunction } from '../utils';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onNext, onComplete, onError, onSubscribe,
+  } = cleanObserver(observer);
+
+  const { source, callable } = this;
+
+  source.subscribeWith({
+    onSubscribe,
+    onComplete() {
+      callable();
+      onComplete();
+    },
+    onError,
+    onNext,
+  });
+}
+
+/**
+ * @ignore
+ */
+export default (source, callable) => {
+  if (!isFunction(callable)) {
+    return source;
+  }
+  const observable = new Observable(subscribeActual);
+  observable.source = source;
+  observable.callable = callable;
+  return observable;
+};
