c89bfedd175dbc23b8a80c9edda8ecaeb2fdf207
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-25 13:53:02 +0800

    Update concatArray.js

--- src/internal/operator/concatArray.js
+++ src/internal/operator/concatArray.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-syntax */
 import { LinkedCancellable } from 'rx-cancellable';
 import Observable from '../../observable';
-import { cleanObserver, isArray } from '../utils';
+import { cleanObserver, isArray, immediateError } from '../utils';
 import error from './error';
 import is from '../is';
 
@@ -16,42 +16,47 @@ function subscribeActual(observer) {
 
   const { sources } = this;
   const { length } = sources;
-  let counter = 0;
 
-  const sub = () => {
-    const source = sources[counter];
-    if (!(is(source))) {
-      onError(new Error('Observable.concat: one of the sources is a non-Observable.'));
-      controller.cancel();
-      return;
-    }
-    controller.unlink();
-    counter += 1;
-    source.subscribeWith({
-      onSubscribe(ac) {
-        controller.link(ac);
-      },
-      onComplete() {
-        if (counter === length) {
-          onComplete();
-          controller.cancel();
-        } else {
-          sub();
-        }
-      },
-      onError,
-      onNext,
-    });
-  };
+  if (length === 0) {
+    immediateError(observer, new Error('Completable.concatArray: sources Array is empty.'));
+  } else {
+    let counter = 0;
 
-  sub();
+    const sub = () => {
+      const source = sources[counter];
+      if (!(is(source))) {
+        onError(new Error('Observable.concatArray: one of the sources is a non-Observable.'));
+        controller.cancel();
+        return;
+      }
+      controller.unlink();
+      counter += 1;
+      source.subscribeWith({
+        onSubscribe(ac) {
+          controller.link(ac);
+        },
+        onComplete() {
+          if (counter === length) {
+            onComplete();
+            controller.cancel();
+          } else {
+            sub();
+          }
+        },
+        onError,
+        onNext,
+      });
+    };
+
+    sub();
+  }
 }
 /**
  * @ignore
  */
 export default (sources) => {
   if (!isArray(sources)) {
-    return error(new Error('Observable.concat: sources is non-Array.'));
+    return error(new Error('Observable.concatArray: sources is non-Array.'));
   }
   const observable = new Observable(subscribeActual);
   observable.sources = sources;
