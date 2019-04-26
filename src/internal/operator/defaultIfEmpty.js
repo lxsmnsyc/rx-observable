e65c005480a3d6dca31227ad61484d9173c81757
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-26 13:38:41 +0800

    Create defaultIfEmpty.js

---
+++ src/internal/operator/defaultIfEmpty.js
@@ -0,0 +1,49 @@
+import { LinkedCancellable } from 'rx-cancellable';
+import { cleanObserver, isNull } from '../utils';
+import Observable from '../../observable';
+
+function subscribeActual(observer) {
+  const {
+    onSubscribe, onNext, onError, onComplete,
+  } = cleanObserver(observer);
+
+  const controller = new LinkedCancellable();
+
+  onSubscribe(controller);
+
+  const { source, value } = this;
+
+  let empty = true;
+
+  source.subscribeWith({
+    onSubscribe(ac) {
+      controller.link(ac);
+    },
+    onComplete() {
+      if (empty) {
+        onNext(value);
+      }
+      onComplete();
+    },
+    onNext(x) {
+      empty = false;
+      onNext(x);
+    },
+    onError,
+  });
+}
+
+/**
+ * @ignore
+ */
+export default (source, value) => {
+  if (isNull(value)) {
+    return source;
+  }
+
+  const observable = new Observable(subscribeActual);
+  observable.source = source;
+  observable.value = value;
+
+  return observable;
+};
