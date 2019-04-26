acd0aafad724bba53eb89c866842059e998ab6d5
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-26 18:16:30 +0800

    Create switchMap.js

---
+++ src/internal/operator/switchMap.js
@@ -0,0 +1,78 @@
+import { LinkedCancellable } from 'rx-cancellable';
+import { isFunction, cleanObserver } from '../utils';
+import Observable from '../../observable';
+import is from '../is';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onNext, onComplete, onError, onSubscribe,
+  } = cleanObserver(observer);
+
+  const controller = new LinkedCancellable();
+  onSubscribe(controller);
+
+  const { source, mapper } = this;
+
+  let origin;
+  let currentStream;
+
+  source.subscribeWith({
+    onSubscribe(c) {
+      controller.link(c);
+      origin = c;
+    },
+    onNext(x) {
+      let result;
+      try {
+        result = mapper(x);
+        if (!is(result)) {
+          throw new Error('Observable.switchMap: mapper function returned a non-Observable.');
+        }
+      } catch (e) {
+        onError(e);
+        return;
+      }
+      controller.unlink();
+      if (currentStream) {
+        currentStream.cancel();
+      }
+      result.subscribeWith({
+        onSubscribe(c) {
+          controller.link(c);
+          currentStream = c;
+        },
+        onComplete() {
+          if (origin.cancelled) {
+            onComplete();
+          } else {
+            controller.link(origin);
+            currentStream = null;
+          }
+        },
+        onError,
+        onNext,
+      });
+    },
+    onComplete() {
+      if (!currentStream) {
+        onComplete();
+      }
+    },
+    onError,
+  });
+}
+/**
+ * @ignore
+ */
+export default (source, mapper) => {
+  if (!isFunction(mapper)) {
+    return source;
+  }
+  const observable = new Observable(subscribeActual);
+  observable.source = source;
+  observable.mapper = mapper;
+  return observable;
+};
