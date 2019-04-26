c0977fc07313396c13bb4a096198e0c48de91bba
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-26 16:51:24 +0800

    Create combineLatest.js

---
+++ src/internal/operator/combineLatest.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-restricted-syntax */
+import { isIterable } from '../utils';
+import error from './error';
+import is from '../is';
+import combineLatestArray from './combineLatestArray';
+
+/**
+ * @ignore
+ */
+export default (sources) => {
+  if (!isIterable(sources)) {
+    return error(new Error('Observable.combineLatest: sources is a non-Iterable.'));
+  }
+  const result = [];
+
+  for (const source of sources) {
+    if (is(source)) {
+      result.push(source);
+    } else {
+      return error(new Error('Observable.combineLatest: one of the sources is not an Observable.'));
+    }
+  }
+
+  return combineLatestArray(result);
+};
