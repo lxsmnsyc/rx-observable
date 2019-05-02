caf6eb375deb0307fe29498b148ffffd2db9bf29
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-02 14:22:07 +0800

    Create takeWhile.js

---
+++ src/internal/operator/takeWhile.js
@@ -0,0 +1,46 @@
+import { LinkedCancellable } from 'rx-cancellable';
+import Observable from '../../observable';
+import { cleanObserver, isFunction } from '../utils';
+
+
+function subscribeActual(observer) {
+  const {
+    onSubscribe, onNext, onError, onComplete,
+  } = cleanObserver(observer);
+
+  const controller = new LinkedCancellable();
+
+  onSubscribe(controller);
+
+  const { source, predicate } = this;
+
+  let flag = true;
+
+  source.subscribeWith({
+    onSubscribe(ac) {
+      controller.link(ac);
+    },
+    onComplete,
+    onError,
+    onNext(x) {
+      if (flag) {
+        flag = predicate(x);
+      }
+      if (flag) {
+        onNext(x);
+      }
+    },
+  });
+}
+/**
+ * @ignore
+ */
+export default (source, predicate) => {
+  if (!isFunction(predicate)) {
+    return source;
+  }
+  const observable = new Observable(subscribeActual);
+  observable.predicate = predicate;
+  observable.source = source;
+  return observable;
+};
